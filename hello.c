a5b0adb6b01b8ff313dddc260b6ad5a9de2970c0
Collin Siracuse <csiracuse2@huskers.unl.edu>
2023-08-27 22:06:29 -0500

    Added name and major

--- hello.c
+++ hello.c
@@ -1,10 +1,17 @@
+/**
+ * Author: Collin Siracuse
+ * Date: 2023/08/27
+ *
+ * A simple name and major program in C
+ *
+ */
+
 #include <stdlib.h>
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-  printf("Name: Collin Siracuse!!!!");
-
-  printf("Major: Computer Science");
+  printf("Name: Collin Siracuse!!!!\n");
+  printf("Major: Computer Science\n");
 
   return 0;
 }
